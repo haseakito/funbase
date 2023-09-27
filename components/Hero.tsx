28d53821c723c9372cde55aaf8693f952dc06f8e
haseakito <haseakito@gmail.com>
2023-09-27 15:26:08 +0800

    :lipstick: Add `Hero`

---
+++ components/Hero.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Logo from '@/public/Funbase.svg'
+import Image from 'next/image'
+
+export function Hero() {
+    return (
+        <div className='mt-20 mx-auto max-w-2xl py-10 md:py-40 lg:py-48'>            
+            <div className='absolute top-10 sm:top-0 md:top-6 p-10 md:w-auto'>
+                <Image
+                    src={Logo}
+                    alt=''                    
+                />
+            </div>
+            <div className='hidden md:mt-40 mb-8 md:flex md:justify-center'>
+                <div className='relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20'>
+                    The very best quality and speed.
+                    <a href="#" className="pl-1 font-semibold text-indigo-600 hover:opacity-80 duration-300">
+                        <span className="absolute inset-0" aria-hidden="true" />
+                        Read more <span>&rarr;</span>
+                    </a>
+                </div>
+            </div>
+            <div className='mt-40 md:mt-5 text-center'>
+                <h1>
+                    The platform where <span className='text-transparent bg-clip-text font-bold bg-gradient-to-br from-purple-300 via-purple-400 to-purple-800'>creators</span> and <span className='text-transparent bg-clip-text font-bold bg-gradient-to-r from-pink-400 to-pink-600'>fans</span> can connect
+                </h1>
+                <p className='mt-2 text-lg leading-8 text-gray-600'>
+                    Join the world of creators and help them succeed throughout their career. <span className='font-semibold text-transparent bg-clip-text bg-gradient-to-br from-yellow-600 to-red-600'>Creatives start with you</span>
+                </p>
+                <div className='mt-10 flex items-center justify-center gap-x-6'>
+                    <button className='bg-blue-400 p-4 rounded hover:rounded-none font-semibold shadow-sm hover:opacity-90 duration-300 focus:outline-1 '>
+                        Get tarted
+                    </button>
+                    <a className='relative group  text-sm font-semibold leading-6 text-gray-900 cursor-pointer'>
+                        Learn More <span>→</span>
+                        <span className='h-1 absolute bottom-0 left-1/2 w-0 bg-black dark:bg-white transition-all duration-500 group-hover:w-1/2'></span>
+                        <span className='h-1 absolute bottom-0 right-1/2 w-0 bg-black dark:bg-white transition-all duration-500 group-hover:w-1/2'></span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
