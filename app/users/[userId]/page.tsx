20b24654d8b12ae1d445207e95cecbb2662cbcbb
haseakito <haseakito@gmail.com>
2023-09-27 15:57:02 +0800

    :lipstick: Add `users` router

---
+++ app/users/[userId]/page.tsx
@@ -0,0 +1,40 @@
+import { Subscription } from '@/components/Subscription'
+import React from 'react'
+
+export default function page() {
+
+    const SubscriptionData = [
+        {
+            id: '1',
+            title: 'Starter',
+            price: 20,
+            description: 'Best option for personal use & for your next project.'
+        }
+    ]
+  return (
+    <section className='bg-white dark:bg-gray-900'>
+        <div className='py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6'>
+            <div className='mx-auto max-w-screen-md text-center mb-8 lg:mb-12'>
+                <h2 className='mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white'>
+                    Subscription Plans
+                </h2>
+                <p className='mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400'>
+                    Subscribe as many as you want! Creators thrive, and you're happy too!
+                </p>
+            </div>
+        </div>
+        <div className='space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0'>
+            { SubscriptionData.map((value, key) => (
+                <Subscription
+                    key={ key }
+                    id={ value.id }
+                    title={ value.title }
+                    price={ value.price }
+                    description={ value.description }
+                />
+            ))}
+        </div>
+        {/* Post */}
+    </section>
+  )
+}
---
+++ app/users/page.tsx
@@ -0,0 +1,41 @@
+import React, { Suspense } from 'react'
+import { Spinner } from '@chakra-ui/react'
+import { ProfileCard } from '@/components/ProfileCard'
+
+export default function page() {
+
+    const ProfileCardData = [
+        {
+            id: '',
+            username: 'David Johnson',
+            profileImage: '',
+            description: 'Glad to see you'
+        }
+    ]
+  return (
+    <section className='m-10 bg-white dark:bg-gray-900'>
+        <div>
+            <h2 className='mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white'>
+                Trending Hot🔥 
+            </h2>
+        </div>
+        <div className=''>
+            <div className='space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0'>
+                { ProfileCardData.map((value, key) => (
+                    <Suspense
+                        key={ key }
+                        fallback={<Spinner thickness='4px' speed='0.65s' emptyColor='gray.200' color='blue.500' size='xl' />}
+                    >
+                        <ProfileCard                            
+                            id={ value.id }
+                            username={ value.username }
+                            profileImage={ value.profileImage }
+                            description={ value.description }
+                        />
+                    </Suspense>
+                ))}
+            </div>
+        </div>
+    </section>
+  )
+}
