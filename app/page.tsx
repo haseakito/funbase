faebb3d2122e451999224a4e6ca2a5dc4415d78e
haseakito <haseakito@gmail.com>
2023-09-27 15:47:09 +0800

    :lipstick: Modified `index` page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,38 @@
 import Image from 'next/image'
+import LandingRight from '@/public/LandingRight.svg'
+import { Hero } from '@/components/Hero'
+import { Testimonial } from '@/components/Testimonial'
+import { Statistic } from '@/components/Statistic'
+import { ContactForm } from '@/components/ContactForm'
+import { Footer } from '@/components/Footer'
+import { Feature } from '@/components/Feature'
 
 export default function Home() {
   return (
-        <main>
-            
+    <div>
+        <main>            
+            <section className='grid lg:grid-cols-2'>                
+                <Hero />                
+                <Image
+                    src={ LandingRight}
+                    alt=''
+                    className='w-[800px] mt-10 hidden lg:block'
+                />
+            </section>
+            <section className='bg-white dark:bg-gray-900'>
+                <Statistic />
+            </section>
+            <section>
+                <Feature />
+            </section>
+            <section className='bg-white dark:bg-gray-900'>
+                <Testimonial />
+            </section>
+            <section className='relative overflow-hidden bg-white dark:bg-gray-900'>
+                <ContactForm />
+            </section>
         </main>
+        <Footer />
+    </div>
     )
 }
