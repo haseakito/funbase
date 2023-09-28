e1bd7597315a3ef6443ce76835333278323134b9
haseakito <haseakito@gmail.com>
2023-09-28 23:28:44 +0800

    :art: Moved under `auth` folder

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -28,7 +28,7 @@ export default function page() {
 
     return (
         <div className='h-full grid md:grid-cols-2 gap-10 mt-10 mx-3 lg:mx-20'>
-            <section className='bg-white dark:bg-slate-900 mt-10'>
+            <section className='bg-white dark:bg-slate-900 lg:mt-10'>
                 <Image
                     src={Logo}
                     alt=''
@@ -36,12 +36,12 @@ export default function page() {
                 />
                 <div>
                     <h2 className='mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
-                        Sign up for absolutely nothing
+                        Sign up for absolutely free
                     </h2>
                     <p className='mt-6 text-lg leading-8 text-gray-600'>
                         Join now the world of creators and get connected with your heros! And explore your infinate possibilities with us!
                     </p>
-                    <dl className='mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none'>
+                    <dl className='mt-16 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none'>
                         {FeatureData.map((value, key) => (
                             <div
                                 key={key}
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -27,7 +27,7 @@ export default function page() {
     ]
     return (
         <div className='h-full grid md:grid-cols-2 gap-10 mt-10 mx-3 lg:mx-20'>
-            <section className='bg-white dark:bg-slate-900 mt-10'>
+            <section className='bg-white dark:bg-slate-900 lg:mt-20'>
                 <Image
                     src={Logo}
                     alt=''
@@ -40,7 +40,7 @@ export default function page() {
                     <p className='mt-6 text-lg leading-8 text-gray-600'>
                         Join now the world of creators and get connected with your heros! And explore your infinate possibilities with us!
                     </p>
-                    <dl className='mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none'>
+                    <dl className='mt-16 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none'>
                         {FeatureData.map((value, key) => (
                             <div
                                 key={key}
