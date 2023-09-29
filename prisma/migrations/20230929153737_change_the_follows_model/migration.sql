dd07a00bf356ac7df227bdae92de334cb486ad5e
haseakito <haseakito@gmail.com>
2023-09-30 03:54:44 +0800

    :card_file_box: Modified `Follows` model

---
+++ prisma/migrations/20230929153737_change_the_follows_model/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Follow` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Follow" DROP CONSTRAINT "Follow_userId_fkey";
+
+-- DropTable
+DROP TABLE "Follow";
+
+-- CreateTable
+CREATE TABLE "Follows" (
+    "followerId" TEXT NOT NULL,
+    "followingId" TEXT NOT NULL,
+
+    CONSTRAINT "Follows_pkey" PRIMARY KEY ("followerId","followingId")
+);
+
+-- AddForeignKey
+ALTER TABLE "Follows" ADD CONSTRAINT "Follows_followerId_fkey" FOREIGN KEY ("followerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Follows" ADD CONSTRAINT "Follows_followingId_fkey" FOREIGN KEY ("followingId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
