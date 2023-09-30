f21c8b183e1084ab13964dfbefaf54f049ef85fa
haseakito <haseakito@gmail.com>
2023-09-30 19:47:23 +0800

    :fire: Removed `getUsers` and `getUser` function and use `prisma` instead

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,30 +2,31 @@ import React, { Suspense } from 'react'
 import { Spinner } from '@/components/Chakra'
 import { ProfileCard } from '@/components/ProfileCard'
 import { User } from '@/utils/types/User'
+import { prisma } from '@/libs/db'
 
-/**
- * Asynchronous function handling getting multiple users
- *  
- * @returns Array of user obejct
- */
-async function getUsers() {
-    // Query the users
-    const res = await fetch(process.env.URL + '/api/user')
+// /**
+//  * Asynchronous function handling getting multiple users
+//  *  
+//  * @returns Array of user obejct
+//  */
+// async function getUsers() {
+//     // Query the users
+//     const res = await fetch(process.env.URL + '/api/user')
 
-    // Check the response status
-    if (!res.ok) {
-        // Redirect to the error.ts
-        throw new Error
-    }
-        // Parse the json body
-        const users = await res.json()
-    return users
-}
+//     // Check the response status
+//     if (!res.ok) {
+//         // Redirect to the error.ts
+//         thrnpmow new Error
+//     }
+//         // Parse the json body
+//         const users = await res.json()
+//     return users
+// }
 
 export default async function page() {
 
     // Fetch multiple users
-    const users: User[] = await getUsers()
+    const users = await prisma.user.findMany()    
 
   return (
     <section className='m-10 bg-white dark:bg-gray-900'>
@@ -44,8 +45,8 @@ export default async function page() {
                         <ProfileCard
                             key={value.id}                            
                             id={ value.id }
-                            name={ value.name }
-                            profileImage={ value.image }
+                            name={ value.name || '' }
+                            profileImage={ value.image || undefined}
                             description=''
                         />
                     </Suspense>
