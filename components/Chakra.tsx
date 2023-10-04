12fa1571654f9aa1d8f8d6622567bb6400502d2b
haseakito <haseakito@gmail.com>
2023-10-04 08:35:17 +0800

    :sparkles: Add `Alert` and `AlertIcon`

--- components/Chakra.tsx
+++ components/Chakra.tsx
@@ -2,26 +2,7 @@
 
 export {
     Spinner,
-    Text,
-    Button,
-    Box,
-    Stack,
-    InputGroup,
-    InputLeftElement,
-    InputRightElement,
-    Input,
-    Tooltip,
-    useToast,
-    useDisclosure,
-    UnorderedList,
-    ListItem,
-    Drawer,
-    DrawerBody,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerOverlay,
-    DrawerContent,    
-    DrawerCloseButton,
-    Divider,
-    AbsoluteCenter
+    Alert,
+    AlertIcon,
+    Button   
 } from '@chakra-ui/react'
