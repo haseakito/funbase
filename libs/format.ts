ab726a395a8ce36a57e730017f8d805a1cd0983c
haseakito <haseakito@gmail.com>
2023-10-08 12:35:50 +0800

    :sparkles: Add datetime formatter

--- libs/format.ts
+++ libs/format.ts
@@ -12,3 +12,21 @@ export const formatMB = (num: number) => {
 export const formatGB = (num: number) => {
     return num * Math.pow(1024, 3)
 }
+
+export const formatDate = (date: string) => {
+    const newDate = new Date(date);
+
+    // Define an array to map month names
+    const monthNames = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    ];
+
+    // Extract the day, month, and year from the Date object
+    const day = newDate.getDate();
+    const month = monthNames[newDate.getMonth()];
+    const year = newDate.getFullYear();
+
+    // Create the formatted date string
+    return `${month} ${day}, ${year}`;
+}
