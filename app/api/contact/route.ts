22aaf38700befa71ff1d4bda98049b007f64ef62
haseakito <haseakito@gmail.com>
2023-10-08 12:54:32 +0800

    :sparkles: Add `contact` api

---
+++ app/api/contact/route.ts
@@ -0,0 +1,49 @@
+import { NextRequest, NextResponse } from "next/server";
+import nodemailer from 'nodemailer'
+
+export async function POST(req: NextRequest) {
+    // Fetch data from contact form
+    const data = await req.json()
+    
+    // Initialize the configs for nodemailer
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.GAMIL_USER,
+            pass: process.env.GMAIL_PASSWORD
+        }
+    })
+
+    // the template for contacts
+    const template = {
+        from: data.email,
+        to: 'haseakito@gmail.com',
+        subject: `Contact from ${ data.name }`,
+        text: `${ data.details } Send from ${ data.name }`,
+        html:
+        `
+        <p>Name</p>
+        ${ data.name }
+        <p>Email Address</p>
+        ${ data.email }
+        <p>Details<p/>
+        ${ data.details }
+        `
+    }
+
+    // Send an email
+    await transporter.sendMail(template)
+    .then(() => {
+        return NextResponse.json('Your contact sent succcessfully!' , {
+            status: 200
+        })
+    })
+    .catch((err) => {
+        // Return an error
+        return NextResponse.json(err, {
+            status: 500
+        })
+    })
+}
